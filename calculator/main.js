d85d875c938a670ed57e98fd05cfe3330dad0f27
karasidnez <karasdnez@gmail.com>
2026-03-24 13:26:38 +0300

    Step 5: refactoring code for calculator

--- calculator/main.js
+++ calculator/main.js
@@ -25,3 +25,10 @@ buttons.forEach((button) => {
     display.value = expression;
   });
 });
+
+import { Calculator } from "./calculator.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const calc = new Calculator();
+  calc.init();
+});
